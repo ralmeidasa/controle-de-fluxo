335db4a0364b5f596999c5c452e9f8023f968e12
Rafael Almeida <rafaelalmeidasa@gmail.com>
2023-07-06 15:49:10 -0300

    Add projeto controle de fluxo.

---
+++ src/Contador.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            //chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro!");
+        }
+    }
+
+    private static class ParametrosInvalidosException extends Exception {
+    }
+
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        if(parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        } else if (parametroUm < parametroDois){
+            int contagem = parametroDois - parametroUm;
+            System.out.println("Contando: ");
+            for (int numero = 1; numero <= contagem; numero++) {
+                System.out.println(numero);
+            }
+        } else {
+            System.out.println("A contagem é 0 (não há números para contar)." );
+        }
+    }
+}
